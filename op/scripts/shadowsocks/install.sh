9e49e28a7a9ed4b29996fe707caf0ba77568ddde
三三 <oott123@users.noreply.github.com>
2015-07-11 15:14:45 +0800

    remove $ from define

--- op/scripts/shadowsocks/install.sh
+++ op/scripts/shadowsocks/install.sh
@@ -17,12 +17,12 @@ wget https://raw.githubusercontent.com/oott123/knowledge/master/op/scripts/shado
 sed -i "s:#ssserver#:`which ssserver`:" /etc/supervisor.d/shadowsocks.ini
 wget https://raw.githubusercontent.com/oott123/knowledge/master/op/scripts/shadowsocks/shadowsocks.json -O /etc/shadowsocks.json
 
-$SSPORT=`shuf -i 2000-65000 -n 1`
-$SSPWD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1`
+SSPORT=`shuf -i 2000-65000 -n 1`
+SSPWD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1`
 read -p "Input password or we will generate one for you: "
 if [[ $REPLY ]]
 then
-  $SSPWD=`echo -e $REPLY | tr -d '[[:space:]]'`
+  SSPWD=`echo -e $REPLY | tr -d '[[:space:]]'`
 fi
 sed -i "s/#port#/$SSPORT/" /etc/shadowsocks.json
 sed -i "s/#password#/$SSPWD/" /etc/shadowsocks.json
